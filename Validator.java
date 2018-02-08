91c74d398fe81fd2b16ea5dcfc99e069ded1d16c
lisakoga <ls256469@dal.ca>
2018-02-07 22:00:21 -0400

    Create Validator.java

---
+++ Validator.java
@@ -0,0 +1,17 @@
+public class Validator
+{
+    public static boolean pwCheck(String pw)
+    {
+        // checks if the password is not “password” (case insensitive)
+        if (pw.toLowerCase() == "password")
+        {
+            return false;
+        }
+        // checks if the password is at least 8 characters long
+        else if (pw.length() < 8)
+        {
+            return false;
+        }
+        return true;
+    }
+}
