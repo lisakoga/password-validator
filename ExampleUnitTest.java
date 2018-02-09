66b47d4b59050aacbc6017ebbe753238447f4947
lisakoga <ls256469@dal.ca>
2018-02-08 22:14:23 -0400

    Create ExampleUnitTest.java

---
+++ ExampleUnitTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class ExampleUnitTest
+{
+    @Test
+    public void isPassword() throws Exception
+    {
+        assertEquals(false, Validator.pwCheck("password"));
+    }
+    @Test
+    public void pwTooShort() throws Exception
+    {
+        assertEquals(false, Validator.pwCheck("qwert"));
+    }
+    @Test
+    public void goodPW() throws Exception
+    {
+        assertEquals(true, Validator.pwCheck("ABCDEF&ghijk890"));
+    }
+}
